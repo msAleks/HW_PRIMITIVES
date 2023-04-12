2e3024b8427a4fb117ba3a754b1edf1d8a24897d
msAleks <aleksandra.96n1@gmail.com>
2023-04-12 18:17:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13_676;
+        int miles = 20;
+
+        int bonus = amount / miles;
+        System.out.println("Количество бонусных миль: " + bonus);
+
+    }
+}
